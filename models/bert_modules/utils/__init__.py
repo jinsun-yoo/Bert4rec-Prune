8e08dbc2a0723c8c45bfa6e4c69c9a792ec7e4d4
JinSun.Yoo <jsyoo1117@gmail.com>
2019-12-01 20:09:49 +0900

    Add . in front of custom_layers at __init__

--- models/bert_modules/utils/__init__.py
+++ models/bert_modules/utils/__init__.py
@@ -1,4 +1,4 @@
 from .feed_forward import PositionwiseFeedForward
-from .layer_norm import LayerNorm
+from .custom_layers import LayerNorm
 from .sublayer import SublayerConnection
 from .gelu import GELU
